fe2ce8952e964a012113cc574cb682a9252e73fb
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-03-05 19:20:05 +0300

    adds leaveRoom function

--- src/socket/index.js
+++ src/socket/index.js
@@ -9,6 +9,7 @@ let currentPeerID = ''
 
 export const newConnectionHandler = socket => {
     
+    
     const socketID = socket.id;
     
     console.log("New connection:", socketID)
@@ -19,11 +20,17 @@ export const newConnectionHandler = socket => {
         roomID = payload.roomID
         currentPeerID = payload.peerID
         socket.join(payload.roomID)
-        socket.to(payload.roomID).emit('user-connected', {peerID: payload.peerID})
+        socket.to(payload.roomID).emit('user-connected', {peerID: payload.peerID, socketID: socketID})
+        
+        
+        socket.on("disconnect", () => {
+            console.log("Client disconnected, socketID:" , socketID, "peerID: ", payload.peerID)
+            // socket.to(roomID).emit('user-disconnect', {socketID: socketID, peerID: currentPeerID}); 
+            leaveRoom(payload.peerID, payload.roomID)
+        })
     })
-    
-    socket.on("disconnect", () => {
-        console.log("Client disconnected, socketID:" , socketID)
-        socket.to(roomID).emit('user-disconnect', {socketID: socketID, peerID: currentPeerID}); 
-    })
+    const leaveRoom =(peerID, roomID) => {
+        //todo: filter updated rooms
+        socket.to(roomID).emit('user-disconnected', {peerID: peerID})
+    }
 }
