7223ae65821c2da3fb6de261858210cd0dd13cf6
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-03-12 14:36:39 +0300

    fixed some bugs

--- src/socket/index.js
+++ src/socket/index.js
@@ -20,17 +20,16 @@ export const newConnectionHandler = socket => {
         roomID = payload.roomID
         currentPeerID = payload.peerID
         socket.join(payload.roomID)
-        socket.to(payload.roomID).emit('user-connected', {peerID: payload.peerID, socketID: socketID})
-        
-        
+        socket.to(payload.roomID).emit('user-connected', {peerID: payload.peerID, socketID: socketID, userID: payload.userID})
+                
         socket.on("disconnect", () => {
             console.log("Client disconnected, socketID:" , socketID, "peerID: ", payload.peerID)
             // socket.to(roomID).emit('user-disconnect', {socketID: socketID, peerID: currentPeerID}); 
-            leaveRoom(payload.peerID, payload.roomID)
+            leaveRoom(payload.peerID, payload.roomID, payload.userID)
         })
     })
-    const leaveRoom =(peerID, roomID) => {
+    const leaveRoom =(peerID, roomID,userID) => {
         //todo: filter updated rooms
-        socket.to(roomID).emit('user-disconnected', {peerID: peerID})
+        socket.to(roomID).emit('user-disconnected', {peerID: peerID, roomID, userID: userID})
     }
 }
